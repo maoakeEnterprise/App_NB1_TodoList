ca5d6023b543fb6fec5514a8b5ec9f232b26bf7f
maoake <maoake.terii@gmail.com>
2021-09-30 21:33:51 +0200

    Test BottomSheetBehavior

--- MyApplicationTodoList/app/src/main/java/com/example/myapplicationtodolist/MainActivity.java
+++ MyApplicationTodoList/app/src/main/java/com/example/myapplicationtodolist/MainActivity.java
@@ -1,44 +1,41 @@
 package com.example.myapplicationtodolist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.LinearLayout;
 
 import com.example.myapplicationtodolist.fragments.HomeFragment;
-import com.example.myapplicationtodolist.fragments.ModalBottomSheetFragment;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity{
 
-    Button test;
-    ModalBottomSheetFragment bottomSheetFragment;
+    private BottomSheetBehavior test;
+    private LinearLayout test2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();
+
         //injecter le fragment dans la boite (fragment container)
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.fragment_container, new HomeFragment());
         transaction.addToBackStack(null);
         transaction.commit();
-        test =(Button)findViewById(R.id.test_bottom_sheet);
-        test.setOnClickListener(this);
+
 
 
     }
 
-
-    public void onClick(View view){
-        Toast.makeText(this, "test",Toast.LENGTH_SHORT);
-        if(view.getId() == test.getId()){
-            Toast.makeText(this, "test",Toast.LENGTH_SHORT);
-            bottomSheetFragment = new ModalBottomSheetFragment();
-            bottomSheetFragment.show(getSupportFragmentManager(), bottomSheetFragment.getTag());
-        }
+    private void init(){
+        this.test2 = findViewById(R.id.bottom_sheet_menu_Container);
+        this.test = BottomSheetBehavior.from(test2);
     }
+
 }
